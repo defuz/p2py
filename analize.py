6f3b6a82c083d4c610a7d4d50543601704776575
defuz <defuz.net@gmail.com>
2012-05-28 15:54:37 +0300

    restruct callist funcs

--- analize.py
+++ analize.py
@@ -10,11 +10,13 @@ def reprCounter(counter):
 	items = [(count, name) for name, count in counter.items()]
 	return '\n'.join("%s: %s" % (n, c) for c, n in reversed(sorted(items)))
 
-def analizeFuncCall(phpNodes):
+def analizeFuncCall(file):
 	try:
-		funcCalls = reduce(add, (node.find('Expr_FuncCall') for node in phpNodes), [])
+		ast = getPHPAst(file)
+		funcCalls = reduce(add, (node.find('Expr_FuncCall') for node in ast), [])
 		return Counter(call.name.parts[0] for call in funcCalls)
 	except: # todo:fix
+		print 'analizeFuncCall talk that something was wrong with file %s ' % file
 		return Counter()
 
 if __name__ == '__main__':
@@ -22,10 +24,5 @@ if __name__ == '__main__':
 	if len(sys.argv) < 2:
 		print("Please specify project pass")
 		sys.exit(1)
-	funcCalls = reduce(add, (analizeFuncCall(getPHPAst(f)) for f in progress(findFiles(sys.argv[1], '.php'))), Counter())
+	funcCalls = reduce(add, (analizeFuncCall(f) for f in progress(findFiles(sys.argv[1], '.php'))), Counter())
 	print reprCounter(funcCalls)
-
-
-
-
-
